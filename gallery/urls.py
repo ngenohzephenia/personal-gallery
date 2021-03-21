ed6f3b833244e5a07f68fbb558043711d797dee7
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 00:11:38 +0300

    craete new regex pattern

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$',views.gallery_of_day,name='galleryToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery')
 ]
