135dc24db2fb6f64ae55755d438bd7e2fe2b3a44
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 00:04:40 +0300

    call convert_dates functions inside gallery_of_day view functions

--- gallery/views.py
+++ gallery/views.py
@@ -8,10 +8,13 @@ def welcome(request):
 
 def gallery_of_day(request):
     date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>Gallery for {day}  {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
