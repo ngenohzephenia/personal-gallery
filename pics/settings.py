99ef960e18c64341a9029cc09cf2dfe1294a6016
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 00:27:00 +0300

    set up my database

--- pics/settings.py
+++ pics/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery',
+    'boostrap3'
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,12 @@ WSGI_APPLICATION = 'pics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pics',
+        'USER': 'moringaaccess',
+    'PASSWORD':'Access',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
