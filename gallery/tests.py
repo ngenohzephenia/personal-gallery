e4ba21a46686fbbc2ecc7a088db04de2bcccecd5
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 08:40:53 +0300

    added a LocationTestClass

--- gallery/tests.py
+++ gallery/tests.py
@@ -11,3 +11,51 @@ class ImageTestClass(TestCase):
 
         self.image_food = Image(name='Kenya', description='there is food', image_location='self.image_location', image_category=self.image_category)
         self.image_food.save_image()
+
+
+     def test_instance(self):
+        self.assertTrue(instance(self.image_food, Image))
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
+    def test_save_image(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    def test_delete_image(self):
+        self.image.save_image()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue((len(images)==0))   
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Mombasa')
+        self.location.save()
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(instance(self.location,Location))
+
+    def test_save_location(self):
+        Location.objects.all().delete()
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_update_location(self):
+        new_name = 'Mombasa'
+        self.location.update_location(self.location.id, name)
+        updated_location = Location.objects.filter(name='Mombasa')
+        self.assertTrue(len(updated_location) > 0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
