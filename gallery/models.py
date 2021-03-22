f36605766f1710a59553b148f2d312df666565b9
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 08:29:35 +0300

    added a save method

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/',default='')
+    name = models.CharField(max_length=200, null=True)
+    image_location = models.ForeignKey('Location',default='', on_delete = models.CASCADE)
+    image_category = models.ForeignKey('Category',default='', on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+    description=models.TextField()
+
+
+    def __str__(self):
+        return self.save()
+
+
+
+    
