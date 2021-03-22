6fab442879109ce5eb5244b67e70417b2475eaf7
ngenohzephenia <ngenohzef@gmail.com>
2021-03-22 08:36:47 +0300

    added a ImageTestClasswith a testing instance

--- gallery/models.py
+++ gallery/models.py
@@ -43,6 +43,21 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name=value)
 
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=80, null= True)
 
     
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.category    
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setup(self):
+        self.image_location = Location(name='Rift-Valley')
+        self.image_loaction.save()
+
+        self.image_category = Category(name='Food')
+        self.image_category.save()
+
+        self.image_food = Image(name='Kenya', description='there is food', image_location='self.image_location', image_category=self.image_category)
+        self.image_food.save_image()
